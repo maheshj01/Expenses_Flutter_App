b9d3bd24f481769a83748d0f1bb2632f2cd6f5be
mahesh jamdade <maheshmn121@gmail.com>
2022-04-05 21:50:36 +0530

    add build files

--- android/app/src/main/java/com/example/expense_manager/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.expense_manager;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
- 
-}
---
+++ android/app/src/main/kotlin/com/wml/expenses_manager/expense_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wml.expenses_manager.expense_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
